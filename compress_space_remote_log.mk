8cc3ad0d940a2b28a0d9b536ee4194be3554fab0
Thierry Delamare <t.delamare@laposte.net>
2017-06-22 11:58:00 +0200

    Adds compress_space_remote_log

---
+++ compress_space_remote_log.mk
@@ -0,0 +1,43 @@
+#!/usr/bin/make -f
+
+top:; @date
+.PHONY: top
+
+self    := $(lastword $(MAKEFILE_LIST))
+sdir    := $(dir $(abspath $(self)))
+$(self) := $(basename $(self))
+name    := $(notdir $($(self)))
+$(self):;
+
+staff := staff
+$(if $(shell getent group $(staff) | grep -q $(USER) || date),$(error $(USER) not in group $(staff)))
+
+b := /space/remote_logs
+
+n ?= 8
+y := $(shell date -d '$n days ago' +%Y)
+m := $(shell date -d '$n days ago' +%m)
+d := $(shell date -d '$n days ago' +%d)
+p := [a-z]*/$y/$m/$d
+
+find  := (cd $b; find $p -maxdepth 0 -type d | sort)
+tgz   := $(find) | xargs -i echo tar czf $b/{}.tgz -C $b {}
+clean := $(find) | xargs -i echo test -s $b/{}.tgz -a -d $b/{} '&&' rm -r $b/{}
+
+trgts := tgz clean
+tgz clean:; @$(strip $($@)) | $(RUN)
+.PHONY: $(trgts)
+
+ifeq ($(dir $(self)),./)
+install_dir  := /usr/local/bin
+install_list := $(self)
+$(install_dir)/%: %; install $< $@; $(if $($*),(cd $(@D); $(strip $(foreach _, $($*), ln -sf $* $_;))))
+install: $(install_list:%=$(install_dir)/%);
+endif
+
+RUN  := cat
+runv := RUN := dash -v
+run  := RUN := dash
+
+vartar := run runv
+$(vartar):; @: $(eval $($@))

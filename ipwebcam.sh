177752f3dde6d69770ec3a4a5d41180591ae5754
Thierry Delamare <t.delamare@laposte.net>
2019-10-11 12:23:43 +0200

    Works on ipwebcam.sh

--- ipwebcam.sh
+++ ipwebcam.sh
@@ -2,26 +2,10 @@
 # https://linux-actif.fr/timelapse
 # https://ubuntuforums.org/showthread.php?t=2022316
 
-date=2019-09-30
-start=16-55-05
-end=19-52-11
-
-date=2019-10-01
-start=05-01-40
-end=22-17-47
-
-first=photo_${date}_$start
-last=photo_${date}_$end
-out=mov_${date}_${start}_${end}
-
 args () { paste <(echo $@ | xargs -n1) <(seq $#) | awk '{ print "local " $1 "=$" $2 }'; }
-
 pipe () { test -p $1 || mknod $1 p; }
-
 input () { cut -d/ -f2 | sort | xargs -i echo file {}; }
-
 list_range () { . <(args p f l); find -maxdepth 1 -name '*.jpg' -newer $f.jpg ! -newer $l.jpg | input > $p; }
-
 list_all () { find -maxdepth 1 -name '*.jpg' | input > $p; }
 
 # -pix_fmt yuvj422p
@@ -49,22 +33,36 @@ interpolate () { ffmpeg -i $1.mp4 -filter minterpolate $1-i.mp4; }
 
 ################
 
-start=2019-10-03T00:00:00
-  end=2019-10-03T06:00:00
+today () { echo $(basename $(pwd)); }
+tomorrow () { date +%F -d "$(today) + 1 day"; }
+next-firsts () { find ../$(tomorrow) -maxdepth 1 -name '*.jpg' | sort | head -${1:-24}; }
+add-next-first () { next-firsts $1 | xargs ln -fst .; }
+point () { echo $(today)T${1}:00; }
 
-start=2019-10-03T20:30:00
-  end=2019-10-03T23:59:00
+init () { mkdir -p .stone .hide; }
+stone () { touch -d $2 .stone/$1; }
+stones () { stone start $(point $1); stone end $(point $2); }
+start () { echo .stone/start; }
+end () { echo .stone/end; }
+tv () { wc -l | tee /dev/stderr | xargs -i echo {}/24 | bc | xargs -i date -d@{} -u +%M:%S; }
+t () { wc -l | xargs -i echo {}/24 | bc | xargs -i date -d@{} -u +%M:%S; }
 
-start () { echo 00-stone/$start; }
-end () { echo 00-stone/$end; }
-stone () { touch -d $1 00-stone/$1; }
-t () { wc -l | tee /dev/stderr | xargs -i echo {}/24 | bc -l; }
+jpgs () { find -maxdepth 1 -name '*.jpg'; }
+find-range () { find -maxdepth 1 -newer $(start) ! -newer $(end); }
+time-all () { jpgs | t; }
+time-range () { find-range | t; }
+time-hiden () { find .hide -name '*.jpg' | t; }
+time-shorten () { find-range | awk NR%2 | t; }
+hide () { find-range | awk NR%2 | xargs mv -t .hide; }
 
-stone $start
-stone $end
+################
 
-find -maxdepth 1 -name '*.jpg' | t
-find 00-rm -name '*.jpg' | t
-find -maxdepth 1 -newer $(start) ! -newer $(end) | t
-find -maxdepth 1 -newer $(start) ! -newer $(end) | awk NR%2 | t
-find -maxdepth 1 -newer $(start) ! -newer $(end) | awk NR%2 | xargs mv -t 00-rm
+jpgs | xargs jpegoptim -pv
+add-next-first
+time-all; stones 00:00 06:00; hide; time-all
+time-range; time-hiden
+time-all; stones 20:20 23:59; hide; time-all
+time-range; time-hiden
+all
+
+################

e8e6d159bdf2cfc079b6727e53d9a4eba3ac7120
Thierry Delamare <t.delamare@laposte.net>
2020-02-02 01:36:30 +0100

    Saves TSV before column

--- flow-matrix.mk
+++ flow-matrix.mk
@@ -9,7 +9,7 @@ data := /usr/local/etc/epi/data/oxa
 data.json: $(data)/ips.json $(data)/groups.json; jq -s . $^ > $@
 
 flow-matrix.tsv: data.json Set.jq flow-matrix.jq flow-matrix-data.jq flow-matrix.mk
-	flow-matrix-data.jq -nr | column -t | tee $@
+	flow-matrix-data.jq -nr | tee $@ | column -t
 
 main: flow-matrix.tsv
 

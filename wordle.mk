9d9e568de41fe7de21fad011f95a979774b393e6
Thierry Delamare <t.delamare@laposte.net>
2022-02-12 18:58:39 +0100

    Fixes wordle.mk

--- wordle.mk
+++ wordle.mk
@@ -18,7 +18,7 @@ $(dir):; mkdir -p $@
 $(dir)/five-letters: $(dict) | $(dir); < $< sed -e "s/'s$$//" | grep '^.....$$' | tr '[:upper:]' '[:lower:]' | sort -u > $@
 $(dir)/five-unique-letters: $(dir)/five-letters; < $< grep -Ev '(.).*\1' > $@
 first: $(dir)/five-unique-letters phony; < $< shuf -n1
-look: $(dir)/five-letters phony; < $< grep $g $(if $e, | grep -v [$e]) $(if $w, | grep '[$w].*[$w]')
+look: $(dir)/five-letters phony; < $< grep $g $(if $e, | grep -v [$e]) $(if $w, | grep '.*[$w].*')
 main: phony $(dir)/five-unique-letters
 
 # Local Variables:
